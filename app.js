40bbc3701ba80046f92a7e359d98189c9b8889b9
Shalini Roy <shaliniroy012@gmail.com>
2015-06-30 18:05:38 +0530

    correct error

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello jenkins checked');
+  res.send('hello jenkins checking');
 });
  
 app.listen(process.env.PORT || 5000);
